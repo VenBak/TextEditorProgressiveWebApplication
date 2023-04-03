8bc81464a0416c80e27e37e1e87d06ddc9666bd1
VenBak <andre.brahin@gmail.com>
2023-04-03 17:16:51 -0400

    added get and put functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const transaction = db.transaction('jate', 'readwrite');
+
+  const store = transaction.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+}
+
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+
+  const transcation = db.transaction('jate', 'readonly');
+
+  const store = transcation.objectStore('jate');
+
+  const request = store.getAll();
+};
 
 initdb();
